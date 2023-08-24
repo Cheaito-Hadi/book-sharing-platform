35d82b7a36826d55ca606e857c3d79c177110da1
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-24 21:20:30 +0300

    Client <> Login Page Design with Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import {Route, Routes} from "react-router";
+import {useEffect, useState} from "react";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div>
-      Book Sharing
-    </div>
-  );
+    const [authenticated, setauthenticated] = useState(null);
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem("authenticated");
+        if (loggedInUser) {
+            setauthenticated(loggedInUser);
+        }
+    }, []);
+    if (!authenticated) {
+        return (
+            <Routes>
+                <Route path="/" element={<Login/>}/>
+            </Routes>
+        );
+    }
+
 }
 
 export default App;
---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,78 @@
+import React, {useState} from "react";
+import "./styles.css";
+import Input from "../../components/Input";
+import axios from "axios";
+import AnimatedIntro from "../../components/AnimatedIntro";
+import Parchment from "../../components/Parchment";
+
+function Login() {
+
+    const [authenticated, setauthenticated] = useState(
+        localStorage.getItem(localStorage.getItem("authenticated") || false)
+    );
+
+    const [credentials, setCredentials] = useState({
+        email: "",
+        password: "",
+    });
+    const handleDataChange = (e) => {
+        setCredentials({...credentials, [e.target.name]: e.target.value});
+    };
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post(
+                "http://127.0.0.1:8000/auth/login",
+                credentials,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            console.log(response)
+            if (response.status === 200) {
+                const responseData = response?.data;
+                if (responseData) {
+                    setauthenticated(true)
+                    localStorage.setItem("authenticated", true);
+                    localStorage.setItem("user", JSON.stringify(responseData));
+                    window.location.reload(false);
+                }
+            }
+        } catch (error) {
+            console.log('error')
+            console.error("Login error:", error);
+        }
+    };
+    return (
+        <div className="container">
+            <div>
+                <AnimatedIntro/>
+            </div>
+            <div className="login-box">
+                <Parchment/>
+                <h2 className="login-heading">Login</h2>
+                <Input
+                    label="Email:"
+                    placeholder="Enter your email"
+                    value={credentials.email}
+                    onChange={handleDataChange}
+                    name="email"
+                    type="text"
+                />
+                <Input
+                    label="Password:"
+                    placeholder="Enter your password"
+                    value={credentials.password}
+                    onChange={handleDataChange}
+                    name="password"
+                    type="password"
+                />
+                <p>Don't Have an account? <a href="/register" className="register-here">Register here!</a></p>
+                <button className="login-btn" onClick={handleLogin}>Login</button>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
