11bd5fee613a8e9af7dd43d1410c13dbbae91841
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-25 00:59:51 +0300

    Client <> Navbar

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react'
+import './styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOutAlt, faBowlFood,faStickyNote } from '@fortawesome/free-solid-svg-icons';
+import {useLocation} from "react-router";
+
+function Navbar() {
+    const location = useLocation(); // once ready it returns the 'window.location' object
+    const [url, setUrl] = useState(null);
+    useEffect(() => {
+        setUrl(location.pathname);
+    }, [location]);
+    function handleLogout() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('authenticated')
+        window.location = '/';
+    }
+
+    return (
+        <nav className="navbar">
+            {/*<img src={Logo} alt="image" className="image-logo"/>*/}
+            <ul>
+                <a href="/" className={(url === "/" ?" active" : "")}><FontAwesomeIcon icon={faBowlFood} />Feeds</a>
+                <a href="/discover-posts" className={(url === "/shopping-list" ?" active" : "")}><FontAwesomeIcon  icon={faStickyNote}/>Discover new Books</a>
+                <a href="/" onClick={handleLogout}><FontAwesomeIcon icon={faSignOutAlt} />Logout</a>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
